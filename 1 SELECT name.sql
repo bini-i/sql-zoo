f3ef17208e7906a0a267861438f0364436d3b438
bini-i <binyam.sh1@gmail.com>
2020-11-16 06:37:50 +0300

    sql zoo

---
+++ 1 SELECT name.sql
@@ -0,0 +1,51 @@
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+SELECT name FROM world
+ WHERE name LIKE '____'
+
+SELECT name
+  FROM world
+ WHERE name = capital
+
+SELECT name
+  FROM world
+ WHERE capital LIKE concat(name, '%city%')
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat('%', name, '%')
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name, '_%')
+
+SELECT name, replace(capital, name, '') AS ext
+FROM world
+WHERE capital LIKE concat(name, '_%')
+
